a288f7a01ed302ddd93880131d914a7e6b0239b6
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-11 18:49:00 +0300

    mainMenu() is added

--- Program.cs
+++ Program.cs
@@ -57,3 +57,104 @@ string[] toThreeAndLess(string[] arr)
 
     return arrThreeAndLess;
 }
+
+void mainMenu()
+{
+    string userChoice = string.Empty;
+    bool getOut = false;
+
+    
+
+    while (getOut != true)
+    {
+        Console.Clear();
+
+        Console.WriteLine("Для работы программы необходим массив строковых переменных,");
+        Console.WriteLine("варианты формирования массива:");
+        Console.WriteLine();
+        Console.WriteLine("1. Воспользоваться тестовым массивом, имеющимся в программе");
+        Console.WriteLine("2. Ввести свой набор слов вручную");
+        Console.WriteLine();
+        Console.WriteLine("Введите выбранный вариант (цифра '1' либо цифра '2') либо 'q' ('Q') для выхода из программы.");
+        Console.WriteLine();
+
+        userChoice = string.Empty;
+        userChoice = Console.ReadLine();
+
+        switch(userChoice)
+        {
+            case "1":
+            {
+                userChoice = string.Empty;
+                Console.WriteLine();
+                Console.WriteLine("Исходный массив:");
+                printArray(strTestArray);
+                Console.WriteLine();
+                strArray = toThreeAndLess(strTestArray);
+                Console.WriteLine("Новый массив, отвечающий требованиям:");
+                printArray(strArray);
+
+                pauseText();
+
+                break;
+            }
+
+            case "2":
+            {
+                Console.WriteLine("Введите массив элементов строкового типа.");
+                Console.WriteLine("Для упрощения задачи достаточно ввести массив как строку, разделяя слова-элементы запятыми");
+                Console.WriteLine("Программа сама сформирует из них массив. Итак, Ваша строка: ");
+                Console.WriteLine();
+
+                text = Console.ReadLine();
+
+                strArray = InputStringArray(text);
+                Console.WriteLine();
+                Console.WriteLine("Введенная строка преобразована в массив: ");
+                printArray(strArray);
+                Console.WriteLine();
+
+                
+                strArray = toThreeAndLess(strArray);
+
+                if(strArray.Length > 0)
+                {
+                    Console.WriteLine("Новый массив, отвечающий требованиям:");
+                    printArray(strArray);
+                }
+                else
+                {
+                    Console.WriteLine("Введенный массив не содержит отвечающих условиям значений.");
+                }
+
+                pauseText();
+                break;
+            }
+
+            case "q":
+            {
+                Console.WriteLine("Программа завершает работу. Всего доброго!");
+                getOut = true;
+                break;
+            }
+        
+            case "Q":
+            {
+                Console.WriteLine("Программа завершает работу. Всего доброго!");
+                getOut = true;
+                break;
+            }
+
+            default:
+            {
+                Console.WriteLine("Ошибочный ввод, попробуйте ввести еще раз:");
+                pauseText();
+                break;
+            }
+        }
+    }
+    Console.WriteLine();
+    Console.Clear();
+
+        
+}
