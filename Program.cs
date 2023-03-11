9065ec009540c1f4c41ac882fc6e32cb7ccc0354
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-11 18:42:55 +0300

    InputStringArray() is added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+string[] strTestArray = new string[8]{"tree","kolobok", "words", "yes", "me", "add", "one", "bear"};
+string[] strArray;
+string text = string.Empty;
+
+//===================================================================================================
+// функция принимает строку из слов, разделенных запятыми и возвращает массив из этих слов,
+string[] InputStringArray(string textArg)                          
+                                    
+{
+    var data = textArg.Split(",") 
+                .Select(e => e = e.Replace(" ", string.Empty))
+               // .Where(e => e.Length <= 3)                                                                    
+                .ToArray(); 
+
+    return data;
+}
