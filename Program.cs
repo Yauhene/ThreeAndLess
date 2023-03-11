5362c99ccd61f5ef017873b8b26a572018425707
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-11 18:47:35 +0300

    toThreeAndLess() is added

--- Program.cs
+++ Program.cs
@@ -31,3 +31,29 @@ void printArray(string[] arr)
    
     Console.WriteLine(strPrint);
 }
+
+//===================================================================================================
+// Функция возвращает новый строковый массив со строками из введенного массива длиной 3 и менее букв
+string[] toThreeAndLess(string[] arr)
+{
+    int tempArrLength = arr.Length;
+    string[] tempArr = new string[arr.Length];
+    int currPos = 0;
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <=3) 
+        {
+            tempArr[currPos] = arr[i];
+            currPos++;   
+        }
+    }
+
+    string[] arrThreeAndLess = new string[currPos];
+    for(int i = 0; i < currPos; i++)
+    {
+        arrThreeAndLess[i] = tempArr[i];
+    }
+
+    return arrThreeAndLess;
+}
